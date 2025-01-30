b54bc9a622f2b090693c955ef98e3be617a20323
sernamalala <sm.sernamalala@gmail.com>
2025-01-30 22:19:47 +0200

    got a working 2 digit calculator

--- script.js
+++ script.js
@@ -39,9 +39,44 @@ function divide(a,b) {
         return a/b;
     }
    
-    return ("Cannot divide by Zero!!");
+    return ("😭😭");
     
     
 }
 
 console.log(calculator(1,0,"/"));
+
+
+//EVENT LISTENERS
+
+const buttons = document.querySelectorAll(".num-button");
+const input = document.getElementById("screen-input");
+const clearButton = document.getElementById("clear");
+const equalButton = document.getElementById("equal");
+clearButton.addEventListener("click",function () {
+    input.value = "";
+    
+})
+
+buttons.forEach((button)=>{
+
+    button.addEventListener("click", function(){
+        input.value += button.innerHTML;
+    })
+
+})
+
+equalButton.addEventListener("click",function() {
+    
+    if(input.value.length>0 && /[+\-*/]/.test(input.value)){
+
+        let string = input.value;
+        let parts = string.split(/([+\-*/])/);
+        let num1 = Number(parts[0]);
+        let op = parts[1];
+        let num2 = Number(parts[2]);
+
+        let result = calculator(num1,num2,op);
+        input.value = result;
+    }
+})
